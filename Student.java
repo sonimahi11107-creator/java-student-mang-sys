5f493c0d3781f694e7c6bc30c8b46c728012482e
Mahi Soni <sonimahi11107@gmail.com>
2025-12-26 00:07:43 +0530

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,14 @@
+public class Student {
+   private int id;
+  private string name;
+  private string surname;
+  private string f_name;
+  private string m_name;
+  private string add;
+
+  //function
+
+  public Student( int id,  string name, string surname, string f_name,string m_name, string add) {
+    
+  
+  
