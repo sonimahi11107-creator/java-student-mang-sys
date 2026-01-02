c88acefd47d73c1c057bdc9ede34960b33bb2a14
Mahi Soni <sonimahi11107@gmail.com>
2026-01-02 13:24:16 +0530

    Add StudentService with list

---
+++ StudentService.java
@@ -0,0 +1,13 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class StudentService{
+
+// arraylist declaration 
+private List<Student> students ; 
+
+  //constructor
+public StudentService() {
+  students = new ArrayList<>();
+}
+}
