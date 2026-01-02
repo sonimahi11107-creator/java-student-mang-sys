73fd4477e9dbb0ad3e8475fe7dd1dcc69f484e76
Mahi Soni <sonimahi11107@gmail.com>
2026-01-02 13:50:50 +0530

    Update StudentService.java

--- StudentService.java
+++ StudentService.java
@@ -3,18 +3,18 @@ import java.util.List;
 
 public class StudentService{
 
-// arraylist declaration 
-private List<Student> students ; 
+    // arraylist declaration
+    private List<Student> students ;
 
-  //constructor
-public StudentService() {
-  students = new ArrayList<>();
-}
+    //constructor
+    public StudentService() {
+        students = new ArrayList<>();
+    }
 
-  //method to add students 
-public void addStudent(Student student){
-  studentd.add(student);
-}
-  
-  
+    //method to add students
+    public void addStudent(Student student){
+        students.add(student);
+    }
+
+
 }
