f70fa765920c7336bd8d686cab765bfc30cc7a8d
Mahi Soni <sonimahi11107@gmail.com>
2026-01-02 13:33:39 +0530

    Create Main.java to test the service

---
+++ Main.java
@@ -0,0 +1,11 @@
+public class Main {
+  public static void main( String[] args ){
+    
+    StudentService service = new StudentService();  //object created
+
+  Student s1 = new Student(402, "Mahi" , "Sunil" , " Neeta " , 90.5 );  // one student detail done
+
+  service.addStudent(s1);
+    System.out.println(s1);
+  }
+}
