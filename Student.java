d40cb4cb103d447af9a78cf5369d9e9289a79153
Mahi Soni <sonimahi11107@gmail.com>
2026-01-02 13:52:30 +0530

    Add toString Method using override Student.java

--- Student.java
+++ Student.java
@@ -1,72 +1,71 @@
 public class Student { // Student class is representing a student entity
-   private int id;
-  private string name;
-  private string f_name;
-  private string m_name;
-  private double percentage;
-//constructor
-  public Student( int id,  string name, string f_name,string m_name, double percentage) {
-     
-         this.id = id;
-         this.name =  name;
-         this.f_name = f_name;
-         this.m_name = m_name;
-         this.percentage = percentage;
+    private int id;
+    private String name;
+    private String f_name;
+    private String m_name;
+    private double percentage;
+    //constructor
+    public Student( int id,  String name, String f_name,String m_name, double percentage) {
 
-  }
-   //methods
-   public int getId(){
-      return id;
-   }
-     public String getName(){
-      return name;
-   }
+        this.id = id;
+        this.name =  name;
+        this.f_name = f_name;
+        this.m_name = m_name;
+        this.percentage = percentage;
 
-   public String getF_name(){
-      return f_name;
-   }
-
-   public String getM_name(){
-      return m_name;
-   }
-   public double getPercentage(){
-      return percentage;
-   }
-
-   // setter to make changes 
-
-   public void setName(String name){
-      this.name = name ;
-   }
-
-   public void setF_name(String f_name ){
-      this.f_name = f_name;
-   }
-
-    public void setM_name(String m_name ){
-      this.m_name = m_name;
+    }
+    //methods
+    public int getId(){
+        return id;
+    }
+    public String getName(){
+        return name;
     }
 
-   public void setPercentage(String percentage){
-      this.percentage = percentage;
-   }
+    public String getF_name(){
+        return f_name;
+    }
 
-   @override 
-   public String toString(){
-      
-      return "ID: " + id + "\n" + " NAME: " + name + "\n" + "FATHER NAME: " + f_name + "\n" + " MOTHER NAME: " +  m_name + "\n" + " PERCENTAGE: " + percentage ;
-   
-   }
-   
-   
-   
-   
-   
-   
+    public String getM_name(){
+        return m_name;
+    }
+    public double getPercentage(){
+        return percentage;
+    }
+
+    // setter to make changes
+
+    public void setName(String name){
+        this.name = name ;
+    }
+
+    public void setF_name(String f_name ){
+        this.f_name = f_name;
+    }
+
+    public void setM_name(String m_name ){
+        this.m_name = m_name;
+    }
+
+    public void setPercentage(double percentage){
+        this.percentage = percentage;
+    }
+
+    @Override
+    public String toString(){
+
+        return "ID: " + id + "\n" + "NAME: " + name + "\n" + "FATHER NAME: " + f_name + "\n" + "MOTHER NAME: " +  m_name + "\n" + "PERCENTAGE: " + percentage ;
+
+    }
+
+
+
+
+
+
 
 
 }
-     
-    
-  
-  
+
+
+
