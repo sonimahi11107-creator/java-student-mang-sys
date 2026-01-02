37bf0bd8981285a9d1b384b8e45c0b48553071f4
Mahi Soni <sonimahi11107@gmail.com>
2026-01-02 12:13:05 +0530

    Add Student class

--- Student.java
+++ Student.java
@@ -1,14 +1,25 @@
 public class Student {
    private int id;
   private string name;
-  private string surname;
   private string f_name;
   private string m_name;
-  private string add;
+  private double percentage;
+
 
   //function
 
-  public Student( int id,  string name, string surname, string f_name,string m_name, string add) {
+  public Student( int id,  string name, string f_name,string m_name, double percentage) {
+     
+         this.id = id;
+         this.name =  name;
+         this.f_name = f_name;
+         this.m_name = m_name;
+         this.percentage = percentage;
+
+  }
+
+}
+     
     
   
   
